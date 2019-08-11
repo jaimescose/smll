c09a75a1c922efe860093ad571bb6056693f1531
Jaime Escobar <jjescobar@uninorte.edu.co>
2019-08-11 00:49:10 -0500

    create register site method

--- apps/sites/models/Site.py
+++ apps/sites/models/Site.py
@@ -1,4 +1,7 @@
 from django.db import models
+import requests
+from bs4 import BeautifulSoup 
+from django.conf import settings
 
 class Site(models.Model):
     title = models.CharField(max_length=200, default = 'No title')
@@ -7,3 +10,28 @@ class Site(models.Model):
 
     def __str__(self):
         return self.url
+
+    @classmethod
+    def register_new_site(cls, url):
+        try:
+            site = cls.objects.get(url = url)
+        except Site.DoesNotExist:
+            response = requests.get(url)
+            html = response.content
+
+            soup = BeautifuSoup(html)
+
+            title = soup.title.string
+
+            site_dict = {
+                'title': title,
+                'url': url
+            }
+
+            site = cls.objects.create(**site_dict)
+
+        short_url = '/'.join([settings.DOMAIN, 'site', str(site.id)])
+
+        return short_url
+
+
