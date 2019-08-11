455c48b70a6f2e009ead83f62702d52ea3671237
Jaime Escobar <jjescobar@uninorte.edu.co>
2019-08-11 16:07:28 -0500

    post and get requests

--- apps/sites/views.py
+++ apps/sites/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views import View
+from django.core import serializers
+import json
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
 
-# Create your views here.
+from apps.sites.models.Site import Site
+
+class SiteView(View):
+    def get(self, request, *args, **kwargs):
+        sites = Site.objects.all()
+        serialized_queryset = serializers.serialize('json', sites)
+
+        response = HttpResponse(serialized_queryset)
+
+        return response
+
+    def post(self, request, *args, **kwargs):
+        body = request.body
+        url = body.get('url')
+
+        site = Site.register_new_site(url)
+
+        response = HttpResponse('developing')
