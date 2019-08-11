50b4479dfa4c26a8791b35ca4be7ed38e39aea15
Jaime Escobar <jjescobar@uninorte.edu.co>
2019-08-11 00:27:35 -0500

    register site model to admin

--- apps/sites/models/Site.py
+++ apps/sites/models/Site.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Site(models.Model):
-    title = models.CharField(max_length=200)
-    url = models.URLField()g
-    visitors = models.IntegerField()
+    title = models.CharField(max_length=200, default = 'No title')
+    url = models.URLField()
+    visitors = models.IntegerField(default = 0)
 
     def __str__(self):
-        return self.title
+        return self.url
